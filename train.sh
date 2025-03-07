4c7951b627b03ed44d80030c9084042fdd239c99
why <879690593@qq.com>
2025-03-07 16:31:11 +0800

    Add additional test comment to train.sh

--- train.sh
+++ train.sh
@@ -2,3 +2,4 @@
 CUDA_VISISBLE_DEVICES=0,1 accelerate launch --config_file='configs/accelerator_config.yaml' tangoflux/train.py   --checkpointing_steps="best" --save_every=5 --config='configs/tangoflux_config.yaml' 
 
 # test git 
+# test git 2
